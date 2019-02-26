56f090c455299d065cab26cc783539f4ef870562
Ryntsevich Oksana <ryntsevich.oksana@gmail.com>
2019-02-26 23:10:01 +0300

    Implement task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(preferences) {
+    var count = 0;
+    for (var i=1; i <= preferences.length; i++){
+        var k = preferences[i-1];
+        var n = preferences[k-1]
+        if (preferences[i-1]===k && preferences[k-1]===n && preferences[n-1]===i){
+            count++;
+        }
+    }
+    return(Math.floor(count/3));
 };
